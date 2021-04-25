ff647a5a030bd327e515cdff6149e24026b46ed2
MaximoDouglas <douglas.h.maximo@gmail.com>
2021-04-25 20:32:53 -0300

    Add main activity instrumented tests

--- app/src/androidTest/java/com/maximo/douglas/oramaapiconsumer/instrumented/InstrumentedSuiteTest.java
+++ app/src/androidTest/java/com/maximo/douglas/oramaapiconsumer/instrumented/InstrumentedSuiteTest.java
@@ -1,11 +1,14 @@
 package com.maximo.douglas.oramaapiconsumer.instrumented;
 
+import com.maximo.douglas.oramaapiconsumer.instrumented.navigation.MainActivityTest;
 import com.maximo.douglas.oramaapiconsumer.instrumented.navigation.NavigationTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(NavigationTest.class)
+@Suite.SuiteClasses(
+        {NavigationTest.class, MainActivityTest.class}
+)
 public class InstrumentedSuiteTest {
 }
---
+++ app/src/androidTest/java/com/maximo/douglas/oramaapiconsumer/instrumented/navigation/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.maximo.douglas.oramaapiconsumer.instrumented.navigation;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.maximo.douglas.oramaapiconsumer.MainActivity;
+import com.maximo.douglas.oramaapiconsumer.R;
+import com.maximo.douglas.oramaapiconsumer.instrumented.BaseInstrumentedTesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.maximo.douglas.oramaapiconsumer.testutils.RecyclerViewMatcher.withRecyclerView;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class MainActivityTest extends BaseInstrumentedTesting {
+
+    @Override
+    public void launchActivity() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void test_if_main_constraint_layout_is_displayed() {
+        onView(withId(R.id.main_activity_constraint_layout)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_if_nav_host_fragment_is_displayed() {
+        onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()));
+    }
+
+}
